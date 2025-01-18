e519d6c72ecdf5b090069207af37a6bc1c9bf817
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-01-19 02:00:28 +0500

    update

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
 "use strict";
 class Person {
     constructor(name, age) {
+        this._name = "";
+        this._age = 0;
         this._name = name;
         this._age = age;
     }
+    seyHello() {
+        return `Salom Men ${this._name} yoshim ${this._age} daman`;
+    }
+}
+class Studend extends Person {
+    constructor(name, age, group, course) {
+        super(name, age);
+        this._group = "";
+        this._course = 0;
+        this._group = group;
+        this._course = course;
+    }
+    seyHello() {
+        const parentText = super.seyHello();
+        return `${parentText} . men ${this._group} da ${this._course} kurs talabasi bolip oqiyman `;
+    }
+}
+class Teacher extends Person {
+    constructor(name, age, darslar) {
+        super(name, age);
+        this._darslar = [];
+        this._darslar = darslar;
+    }
     info() {
-        return `${this._name} ${this._age} yoshda`;
-    }
-    static description() {
-        return `bu class haqida malumot beradigan cali soz static`;
-    }
-    get name() {
-        return this._name;
-    }
-    set name(value) {
-        this._name = value;
-    }
-    get age() {
-        return this._age;
-    }
-    set age(value) {
-        if (value > 0 && value < 1000) {
-            this._age = value;
-        }
-        else {
-            console.log("siz kiritgan yosh togri kelmaydi ");
-        }
+        return `men ${this._darslar[0]} fanidan dars beraman `;
     }
 }
-Person.isFlaying = false;
 const Javohir = new Person("Javohir", 20);
-// const Shox = new Person("Shox", 20);
 console.log(Javohir);
-Javohir.name = "Suvonov";
-Javohir.age = 21;
-console.log(Javohir);
-// console.log(Shox)
-// console.log(Person.isFlaying)
-// console.log(Person.description())
-if (Javohir instanceof Person) {
-    console.log("Tegishli");
-}
-else {
-    console.log("Tegishli emas");
-}
+console.log(Javohir.seyHello());
+const Suvonov = new Studend("Suvonov", 20, "17-23 guruh", 2);
+console.log(Suvonov);
+console.log(Suvonov.seyHello());
+const newStudent = Suvonov;
+console.log(newStudent);
+console.log(newStudent.seyHello());
+const teacher = new Teacher("Shuhrat", 26, ["Fizika", "AI"]);
+console.log(teacher);
+console.log(teacher.seyHello());
+console.log(teacher.info());
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,51 +1,63 @@
-class Person{
-    _name:string ;
-    _age : number ;
+class Person {
+  _name: string = "";
+  _age: number = 0;
 
-    constructor(name:string,  age:number){
-        this._name = name;
-        this._age = age
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
+  }
+
+  seyHello():string{
+    return `Salom Men ${this._name} yoshim ${this._age} daman`
+  }
+}
+
+class Studend extends Person {
+  _group: string = "";
+  _course: number = 0;
+
+  constructor(name: string, age: number, group: string, course: number) {
+    super(name, age);
+    this._group = group;
+    this._course = course;
+  }
+
+  seyHello():string{
+    const parentText = super.seyHello()
+    return `${parentText} . men ${this._group} da ${this._course} kurs talabasi bolip oqiyman `
+  }
+
+  
+}
+
+class Teacher extends Person{
+    _darslar :string[] =[];
+    
+    constructor(name:string, age:number, darslar : string[]){
+        super(name, age);
+        this._darslar = darslar
     }
+
     info():string{
-        return `${this._name} ${this._age} yoshda`
-    }
-    static isFlaying = false
-    static description():string{
-        return `bu class haqida malumot beradigan cali soz static`
-    }
-
-    get name():string{
-        return this._name
-    }
-
-    set name(value:string){
-        this._name = value
-    }
-    get age():number{
-        return this._age
-    }
-    set age(value:number){
-        if(value >0 && value<1000){
-          this._age = value
-        }else{
-            console.log("siz kiritgan yosh togri kelmaydi ")
-        }
+        return `men ${this._darslar[0]} fanidan dars beraman `
     }
 }
 
-const Javohir = new Person("Javohir", 20);
-// const Shox = new Person("Shox", 20);
+const Javohir: Person = new Person("Javohir", 20);
+console.log(Javohir);
+console.log(Javohir.seyHello());
 
-console.log(Javohir)
-Javohir.name = "Suvonov";
-Javohir.age = 21
-console.log(Javohir)
-// console.log(Shox)
-// console.log(Person.isFlaying)
-// console.log(Person.description())
+const Suvonov: Studend = new Studend("Suvonov" , 20, "17-23 guruh", 2);
+console.log(Suvonov)
+console.log(Suvonov.seyHello())
 
-if(Javohir instanceof Person){
-    console.log("Tegishli")
-}else{
-    console.log("Tegishli emas")
-}
+const newStudent : Person = <Person>Suvonov;
+console.log(newStudent)
+console.log(newStudent.seyHello())
+
+const teacher:Teacher = new Teacher("Shuhrat", 26, ["Fizika", "AI"])
+console.log(teacher) 
+console.log(teacher.seyHello()) 
+console.log(teacher.info()) 
+
+
